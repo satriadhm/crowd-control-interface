5d8685647632cae83ec0befb16338562b363a17a
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2025-01-24 13:39:06 +0700

    fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,24 @@
-'use client';
-import Link from 'next/link';
+"use client";
 
 export default function Dashboard() {
-    return (
-        <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold mb-8">Dashboard</h1>
-            <Link href="/eval">
-                <button className="px-6 py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition">
-                    Go to Test Page
-                </button>
-            </Link>
-        </div>
-    );
+  const navigateTo = (path: string) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      window.location.href = `${path}?creds=${token}`;
+    } else {
+      window.location.href = "/login";
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-8">Dashboard</h1>
+    <button
+      onClick={() => navigateTo("/eval")}
+      className="px-6 py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
+    >
+      Go to Test Page
+    </button>
+    </div>
+  );
 }
