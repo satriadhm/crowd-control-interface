076a74a661b59a933ebbbddb0002ea6e106b553f
satriadhm <glorioussatria@gmail.com>
2025-03-24 21:32:43 +0700

    feat: test result page

--- src/graphql/queries/auth.ts
+++ src/graphql/queries/auth.ts
@@ -4,6 +4,8 @@ export const GET_LOGGED_IN_USER = gql`
   query GetLoggedInUser($token: String!) {
     me(token: $token) {
       id
+      isEligible
+      isDoneTaskIds
       userName
       firstName
       lastName
