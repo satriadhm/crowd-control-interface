39ddb7ec2f288601e40fce34f92795f42d78ff3e
satriadhm <glorioussatria@gmail.com>
2025-05-30 14:06:13 +0700

    fix

--- src/components/sections/TractionRoadmapSection.tsx
+++ src/components/sections/TractionRoadmapSection.tsx
@@ -46,14 +46,6 @@ const TractionRoadmapSection = ({ sectionClasses }) => {
                   <p className="text-sm text-gray-300">2 software companies committed to pilot program</p>
                 </div>
               </li>
-              
-              <li className="flex items-start gap-3">
-                <div className="bg-tertiary/20 p-2 rounded-full mt-1">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-tertiary-light">
-                    <path d="M20 6 9 17l-5-5" />
-                  </svg>
-                </div>
-              </li>
             </ul>
           </div>
           
