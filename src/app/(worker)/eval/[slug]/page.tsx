b09eed945d1be116585212c8d5723e62693f3901
satriadhm <glorioussatria@gmail.com>
2025-06-19 14:10:35 +0700

    fix

--- src/app/(worker)/eval/[slug]/page.tsx
+++ src/app/(worker)/eval/[slug]/page.tsx
@@ -7,7 +7,7 @@ export default function PageDetail() {
   const taskId = useTaskDetail((state) => state.id);
 
   return (
-    <div className="flex h-screen w-full justify-center items-center bg-gradient-to-r from-[#0a1e5e] to-[#001333] text-white">
+    <div className="min-h-screen w-full flex justify-center items-start py-6 px-4 bg-gradient-to-r from-[#0a1e5e] to-[#001333] text-white">
       <TaskEvaluation taskId={taskId} />
     </div>
   );
